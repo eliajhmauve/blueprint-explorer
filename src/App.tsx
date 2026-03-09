11310bd0d4b702eb229112fb8ee10eb2f097278c
WenKai Shi <73520560+eliajhmauve@users.noreply.github.com>
2026-03-09 21:52:27 +0800

    fix: add basename to Router for GitHub Pages subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/blueprint-explorer">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/random" element={<RandomMode />} />
